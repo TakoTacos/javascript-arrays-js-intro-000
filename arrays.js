a0625eac9dac1d9c57cc74cd790827a4885dca11
Corey Lewis <clewis.development@gmail.com>
2018-05-12 04:42:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,6 +19,6 @@ function addElementToEndOfArray(array, element) {
  return [...array, element]
 }
 
-function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element)
-  return array
+function destructivelyAddElementToEndOfArray() {
+
+}
