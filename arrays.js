d9b6fc7db30cb67a62b65d685d2c50c7ce0d8d01
Corey Lewis <clewis.development@gmail.com>
2018-05-12 04:31:54 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kit kat',
+  'skittles'
+]
+
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+
+}
+
+function addElementToEndOfArray() {
+
+}
+
+function destructivelyAddElementToEndOfArray() {
+
+}
